78c01401e4c17efbe829060141f021bb0eb0a6e3
shine_52 <156677365+Shineattryb@users.noreply.github.com>
2024-02-16 16:11:16 +0530

    Add files via upload

--- components/alert_header.js
+++ components/alert_header.js
@@ -1,19 +1,35 @@
-import React from 'react'
+import React ,{useState}from 'react'
 import './alert_header.css';
+
+import {
+  AlertPopup,
+  AlertPopupHeader,
+  AlertPopupBody,
+  AlertPopupFooter,
+  Button,
+  useModal,
+} from "@attrybtech/attryb-ui";
 function Alert_header({svglink2,svglink3}) {
+  const { modalState, modalRef, exitModal, showModal } = useModal();
+  function modalCancelHandler() {
+    console.log("Cancelled");
+    exitModal();
+}
+
   return (
+    
     <div className='header'>
-      {/* background-image */}
-    {/* <img src= {svglink1} alt="background"/> */}
+    
+    
 
 {/* call-icon */}
 <div className='icons'>
  
 <img src={svglink2} id="call"alt="icons-image"/>
 {/* cross-icon */}
-
-<img src={svglink3} id="cross" alt="cross-icon"/>
-
+<a href="#">
+<img src={svglink3} id="cross" alt="cross-icon" onClick={modalCancelHandler}/>
+</a>
 </div>
 
 
